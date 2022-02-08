50b6465b2a08efe341863e6643aada94a3d67a11
GreenJon902 <GreenJon902@gmail.com>
2022-02-08 16:23:44 +0000

    Initial commit

---
+++ src/main/java/com/greenjon902/coloredchat/ColoredChat.java
@@ -0,0 +1,17 @@
+package com.greenjon902.coloredchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ColoredChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
