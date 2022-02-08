481de30aee966dc95f7279e80b7ad3dbb0734972
GreenJon902 <GreenJon902@gmail.com>
2022-02-08 17:35:36 +0000

    added command parts

--- src/main/java/com/greenjon902/coloredchat/ColoredChat.java
+++ src/main/java/com/greenjon902/coloredchat/ColoredChat.java
@@ -2,12 +2,14 @@ package com.greenjon902.coloredchat;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class ColoredChat extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        Objects.requireNonNull(this.getCommand("ColoredChat")).setExecutor(new ColoredChatCommands());
     }
 
     @Override
---
+++ src/main/java/com/greenjon902/coloredchat/ColoredChatCommands.java
@@ -0,0 +1,21 @@
+package com.greenjon902.coloredchat;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class ColoredChatCommands implements TabExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return false;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return null;
+    }
+}
