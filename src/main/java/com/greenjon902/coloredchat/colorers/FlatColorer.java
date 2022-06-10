5b96f5fc07807e1b9bcd251c99b164c34cbda07d
greenjon902 <greenjon902@gmail.com>
2022-06-10 20:50:42 +0100

    added FlatColorer contents

--- src/main/java/com/greenjon902/coloredchat/colorers/FlatColorer.java
+++ src/main/java/com/greenjon902/coloredchat/colorers/FlatColorer.java
@@ -3,11 +3,20 @@ package com.greenjon902.coloredchat.colorers;
 import org.bukkit.Color;
 
 public class FlatColorer implements Colorer {
+    private Color color;
+    public Color getColor() {
+        return color;
+    }
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
     public FlatColorer(Color color) {
+        this.color = color;
     }
 
     @Override
     public String color(String string) {
-        return null;
+        return color + string;
     }
 }
