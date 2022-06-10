8207a196c63485b91b129bf3b2458850c54b9349
greenjon902 <greenjon902@gmail.com>
2022-06-10 20:40:11 +0100

    fixed arg parsing and some tab filling

--- src/main/java/com/greenjon902/coloredchat/ColoredChatCommands.java
+++ src/main/java/com/greenjon902/coloredchat/ColoredChatCommands.java
@@ -6,6 +6,7 @@ import com.greenjon902.coloredchat.colorers.GradientColorer;
 import com.greenjon902.coloredchat.colorers.RainbowColorer;
 import com.greenjon902.utils.FilterList;
 import org.apache.commons.lang.NotImplementedException;
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -27,7 +28,7 @@ public class ColoredChatCommands implements TabExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(args.length > 1)) {
-            sender.sendMessage(Color.RED + "Not enough arguments, takes at least 2");
+            sender.sendMessage(ChatColor.RED + "Not enough arguments, takes at least 2");
             return true;
         }
         String useCase = args[0];
@@ -35,11 +36,11 @@ public class ColoredChatCommands implements TabExecutor {
         Colorer colorer;
 
         if (!firstArgSet.contains(useCase)) {
-            sender.sendMessage(Color.RED + "First argument must be on of these - " + firstArgSet);
+            sender.sendMessage(ChatColor.RED + "First argument must be on of these - " + firstArgSet);
             return true;
         }
         if (!secondArgSet.contains(colorMode)) {
-            sender.sendMessage(Color.RED + "Second argument must be on of these - " + secondArgSet);
+            sender.sendMessage(ChatColor.RED + "Second argument must be on of these - " + secondArgSet);
             return true;
         }
 
@@ -48,24 +49,26 @@ public class ColoredChatCommands implements TabExecutor {
         switch (colorMode) {
             case "flatcolor":
                 if (args.length != 3) {
-                    sender.sendMessage(Color.RED + "/coloredchat ... flatcolor takes 3 arguments");
+                    sender.sendMessage(ChatColor.RED + "/coloredchat ... flatcolor takes 3 arguments");
                     return true;
                 }
                 sColor = args[2];
                 if (!sColor.startsWith("#")) {
-                    sender.sendMessage(Color.RED + "Color has to start with with #");
+                    sender.sendMessage(ChatColor.RED + "Color has to start with with #");
                     return true;
                 }
                 try {
                     iColor = Integer.parseInt(sColor.replaceFirst("#", ""), 16);
                 } catch (NumberFormatException e) {
-                    sender.sendMessage(Color.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
+                    e.printStackTrace();
+                    sender.sendMessage(ChatColor.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
                     return true;
                 }
                 colorer = new FlatColorer(Color.fromRGB(iColor));
+                break;
             case "rainbow":
                 if (!(args.length == 2 || args.length == 3)) {
-                    sender.sendMessage(Color.RED + "/coloredchat ... rainbow takes 2 or 3 arguments");
+                    sender.sendMessage(ChatColor.RED + "/coloredchat ... rainbow takes 2 or 3 arguments");
                     return true;
                 }
                 if (args.length == 2) {
@@ -75,56 +78,58 @@ public class ColoredChatCommands implements TabExecutor {
                     try {
                         resolution = Integer.parseInt(args[2]);
                     } catch (NumberFormatException e) {
-                        sender.sendMessage(Color.RED + "Invalid number - \"" + args[2] + "\"");
+                        sender.sendMessage(ChatColor.RED + "Invalid number - \"" + args[2] + "\"");
                         return true;
                     }
                     colorer = new RainbowColorer(resolution);
                 }
+                break;
             case "gradient":
                 if (!(args.length >= 3)) {
-                    sender.sendMessage(Color.RED + "/coloredchat ... gradient takes 3 or more arguments");
+                    sender.sendMessage(ChatColor.RED + "/coloredchat ... gradient takes 3 or more arguments");
                     return true;
                 }
-                if ((args.length % 2) == 1) {
-                    sender.sendMessage(Color.RED + "/coloredchat ... gradient takes an even number of args");
-                    return true;
-                }
-                if (args[3].startsWith("#")) { // all args are colors
+                if (args.length == 3 || args[3].startsWith("#")) { // all args are colors
                     Color[] colors = new Color[args.length-2];
                     for (int i=2; i<args.length; i++) {
                         sColor = args[i];
                         if (!sColor.startsWith("#")) {
-                            sender.sendMessage(Color.RED + "Color has to start with with #");
+                            sender.sendMessage(ChatColor.RED + "Color has to start with with #");
                             return true;
                         }
                         try {
                             iColor = Integer.parseInt(sColor.replaceFirst("#", ""), 16);
                         } catch (NumberFormatException e) {
-                            sender.sendMessage(Color.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
+                            sender.sendMessage(ChatColor.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
                             return true;
                         }
                         colors[i-2] = Color.fromRGB(iColor);
                     }
                     colorer = new GradientColorer(colors);
                 } else { // not all args are colors
+                    if ((args.length % 2) == 1) {
+                        sender.sendMessage(ChatColor.RED + "/coloredchat ... gradient takes an even number of args");
+                        return true;
+                    }
+                    
                     GradientColorer.ColorAndAmount[] colorsAndAmounts = new GradientColorer.ColorAndAmount[args.length-2];
                     for (int i=2; i<args.length; i+=2) {
                         sColor = args[i];
                         if (!sColor.startsWith("#")) {
-                            sender.sendMessage(Color.RED + "Color has to start with with #");
+                            sender.sendMessage(ChatColor.RED + "Color has to start with with #");
                             return true;
                         }
                         try {
                             iColor = Integer.parseInt(sColor.replaceFirst("#", ""), 16);
                         } catch (NumberFormatException e) {
-                            sender.sendMessage(Color.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
+                            sender.sendMessage(ChatColor.RED + "Color can only contain characters 1,2,3,4,5,6,7,8,9,a,b,c,d,e,f");
                             return true;
                         }
                         int amount;
                         try {
                             amount = Integer.parseInt(args[i+1]);
                         } catch (NumberFormatException e) {
-                            sender.sendMessage(Color.RED + "Invalid number - \"" + args[i+1] + "\"");
+                            sender.sendMessage(ChatColor.RED + "Invalid number - \"" + args[i+1] + "\"");
                             return true;
                         }
                         colorsAndAmounts[i-2] = new GradientColorer.ColorAndAmount(Color.fromRGB(iColor), amount);
@@ -152,14 +157,7 @@ public class ColoredChatCommands implements TabExecutor {
                 colorMode = args[1];
                 switch (colorMode) {
                     case "flatcolor":
-                    case "rainbow":
                         return FilterList.filterListByStart(thirdArgSetA, args[2]);
-                }
-            case 4:
-                colorMode = args[1];
-                switch (colorMode) {
-                    case "flatcolor":
-                        return null;
                     case "rainbow":
                         return FilterList.filterListByStart(thirdArgSetB, args[2]);
                 }
