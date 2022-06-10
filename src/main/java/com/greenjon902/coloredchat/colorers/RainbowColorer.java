b0000d1102ca08f9fbbdf0001039e87d7e57d304
greenjon902 <greenjon902@gmail.com>
2022-06-10 21:05:05 +0100

    added RainbowColorer contents

--- src/main/java/com/greenjon902/coloredchat/colorers/RainbowColorer.java
+++ src/main/java/com/greenjon902/coloredchat/colorers/RainbowColorer.java
@@ -1,14 +1,34 @@
 package com.greenjon902.coloredchat.colorers;
 
+import org.bukkit.Color;
+
 public class RainbowColorer implements Colorer {
+    private static final Color[] colors = new Color[] {
+            Color.fromRGB(0xFF0000),
+            Color.fromRGB(0xFF7F00),
+            Color.fromRGB(0xFFFF00),
+            Color.fromRGB(0x00FF00),
+            Color.fromRGB(0x0000FF),
+            Color.fromRGB(0x4B0082),
+            Color.fromRGB(0x9400D3)
+    };
+
+    private GradientColorer gradientColorer;
+
     public RainbowColorer(int resolution) {
+        GradientColorer.ColorAndAmount[] colorsAndAmounts = new GradientColorer.ColorAndAmount[colors.length];
+        for (int i=0; i<colorsAndAmounts.length; i++) {
+            colorsAndAmounts[i] = new GradientColorer.ColorAndAmount(colors[i], resolution);
+        }
+        gradientColorer = new GradientColorer(colorsAndAmounts);
     }
 
     public RainbowColorer() {
+        gradientColorer = new GradientColorer(colors);
     }
 
     @Override
     public String color(String string) {
-        return null;
+        return gradientColorer.color(string);
     }
 }
